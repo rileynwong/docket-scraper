b7050425fd5355c14e5fbe9e7850d5451dbc1237
Riley Wong <nancypwong@gmail.com>
2018-07-15 14:51:31 -0400

    Sample outputs. Remove output files after testing

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 
 import requests
 
+# TODO iterate through all dockets
 url = 'https://ujsportal.pacourts.us/DocketSheets/MDJReport.ashx?docketNumber=MJ-51301-CR-0000010-2018'
 response = requests.get(url, stream=True)
 
