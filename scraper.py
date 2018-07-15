37847e3b8822847f0891fc57a4ad524faa014cd8
Riley Wong <nancypwong@gmail.com>
2018-07-15 14:10:19 -0400

    Test downloading PDF using requests

---
+++ scraper.py
@@ -0,0 +1,9 @@
+### Scrape PDFs from UJS Portal.
+
+import requests
+
+url = 'https://ujsportal.pacourts.us/DocketSheets/MDJReport.ashx?docketNumber=MJ-51301-CR-0000010-2018'
+response = requests.get(url, stream=True)
+
+with open ('test.pdf', 'wb') as f:
+    f.write(response.content)
